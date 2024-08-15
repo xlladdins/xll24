6603b827fe9a08324d8bc284983df2c1d68077e1
Keith A. Lewis <kal@kalx.net>
2024-08-15 07:08:34 -0400

    setup

--- Templates/ProjectTemplates/Visual C++/template.h
+++ Templates/ProjectTemplates/Visual C++/template.h
@@ -1,6 +1,6 @@
 // $(ProjectName).h - Header file to include xll.h and define CATEGORY
 #pragma once
-#include "xll.h"
+#include "include/xll.h"
 
 #ifndef CATEGORY // Function wizard category
 #define CATEGORY "XLL"
