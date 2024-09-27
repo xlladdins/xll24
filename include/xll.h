62e1f9e114bd31a7afddd104838da8d7735b583e
Keith A. Lewis <kal@kalx.net>
2024-09-27 19:12:02 -0400

    statt adding runtime add-in information

--- include/xll.h
+++ include/xll.h
@@ -10,15 +10,36 @@
 #include "excel_time.h"
 #include "enum.h"
 
-// Set or return value of xlAddInManagerInfo.
-inline LPXLOPER12 AddInManagerInfo(const XLOPER12& info = xll::Nil)
-{
-	static xll::OPER x;
+namespace xll {
 
-	if (xll::type(info) == xltypeStr) {
-		x = info;
+	// Set or return value of xlAddInManagerInfo.
+	inline LPXLOPER12 AddInManagerInfo(const XLOPER12& info = xll::Nil)
+	{
+		static xll::OPER x;
+
+		if (xll::type(info) == xltypeStr) {
+			x = info;
+		}
+
+		return &x;
 	}
 
-	return &x;
-}
+	// Use defined names for runtime information about all loaded add-ins.
+	struct AddInInfo {
+		// Full path to xll.
+		static const OPER& GetName()
+		{
+			static const OPER name = Excel(xlGetName);
+
+			return name;
+		}
+		static OPER Modules()
+		{
+			static OPER modules(L"Modules");
+
+			return Excel(xlfGetName, modules);
+		}
+	};
+
+} // namespace xll
 
