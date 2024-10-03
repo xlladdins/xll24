7702119cb6ae5ce18e957e258560c620b7d3f47e
Keith A. Lewis <kal@kalx.net>
2024-10-03 18:22:58 -0400

    sync

--- src/py.cpp
+++ src/py.cpp
@@ -1,6 +1,5 @@
-// py.cpp - Generate Python ctypes module.
-// Use Python[`ctypes`](https://docs.python.org/3/library/ctypes.html)
-// to load and specify the signature of `xll:: AddIn` functions with .Python() to generate Python code.
+// py.cpp - Generate Python module using ctypes.
+// https://docs.python.org/3/library/ctypes.html
 // The macro `PY` generates a Python module for import.
 #include <fstream>
 #include <map>
@@ -27,34 +26,28 @@ using namespace xll;
 	X(LONG_,    N, N,  c_void_p, c_void_p)  \
 	X(LPOPER,   P, Q,  c_void_p, POINTER(OPER))    \
 	X(LPXLOPER, R, U,  c_void_p, POINTER(OPER))    \
-	X(UINT,     H, H,  c_uint, c_uint)     \
-	X(INT,      J, J,  c_int, c_int)       \
-
-//	X(VOLATILE, !, !,  called every time sheet is recalculated)            \
-//	X(UNCALCED, #, #,  dereferencing uncalculated cells returns old value) \
-//	X(VOID,     ,  >,  return type to use for asynchronous functions)      \
-//	X(THREAD_SAFE,  , $, declares function to be thread safe)              \
-//	X(CLUSTER_SAFE, , &, declares function to be cluster safe)             \
-//	X(ASYNCHRONOUS, , X, declares function to be asynchronous)             \
+	X(UINT,     H, H,  c_uint, c_uint)      \
+	X(INT,      J, J,  c_int, c_int)        \
+	X(VOID,     >, >,  None)                \
 
 namespace py {
 
 	// Excel to Python types.
 	inline std::map<xll::OPER, std::wstring> ctype = {
 	#define PY_ARG_CTYPE(T, A, B, C, D) { xll::OPER(L#B), L#C },
-				PY_ARG_TYPE(PY_ARG_CTYPE)
-		#undef PY_ARG_CTYPE
+		PY_ARG_TYPE(PY_ARG_CTYPE)
+	#undef PY_ARG_CTYPE
 	#define PY_ARG_CTYPE(T, A, B, C, D) { xll::OPER(L#C), L#D },
-				PY_ARG_TYPE(PY_ARG_CTYPE)
-		#undef PY_ARG_CTYPE
+		PY_ARG_TYPE(PY_ARG_CTYPE)
+	#undef PY_ARG_CTYPE
 	};
 
 	//
 	// Excel data types.
 	//
 
-	constexpr const char* XLREF12 =
-		R"(class XLREF12(Structure):
+	constexpr const char* XLREF12 = R"(
+class XLREF12(Structure):
 	_fields_ = [
 		("rwFirst", c_int),
 		("rwLast", c_int),
@@ -62,24 +55,23 @@ namespace py {
 		("colLast", c_int),
 	]
 )";
-	constexpr const char* SRef =
-		R"(class SRef(Structure):
+	constexpr const char* SRef = R"(
+class SRef(Structure):
 	_fields_ = [
 		("count", c_ushort),
 		("ref", XLREF12),
 	]
 )";
 
-	constexpr const char* MRef =
-		R"(class MRef(Structure):
+	constexpr const char* MRef = R"(
+class MRef(Structure):
 	_fields_ = [
 		("count", c_ushort),
 		("reftbl", SRef), # only one SRef allowed
 	]
 )";
 
-	constexpr const char* Array =
-		R"(
+	constexpr const char* Array = R"(
 class OPER(Structure):
 	pass
 
@@ -91,8 +83,8 @@ class Array(Structure):
 	]
 )";
 
-	constexpr const char* Val =
-		R"(class Val(Union):
+	constexpr const char* Val = R"(
+class Val(Union):
 	_fields_ = [
 		("num", c_double),
 		("str", c_wchar_p),
@@ -106,8 +98,7 @@ class Array(Structure):
 	]
 )"; // TODO: define bigdata
 
-	constexpr const char* OPER =
-		R"(
+	constexpr const char* OPER = R"(
 OPER._fields_ = [
 	("val", Val),
 	("xltype", c_ushort),
@@ -127,25 +118,25 @@ def install_root():
 		return None
 )";
 
-	// Replace .xll% with .py.
-	inline std::wstring file_py(const xll::OPER& module)
+	// Replace .xll$ with .py.
+	inline std::wstring file_py(const xll::OPER& name)
 	{
-		std::wstring file = module.to_wstring();
+		std::wstring file = name.to_wstring();
 		ensure(file.ends_with(L".xll"));
 		file.replace(file.size() - 4, 4, L".py");
 
 		return file;
 	}
 	// Base name of module.
-	inline std::wstring module_py(const xll::OPER& module)
+	inline std::wstring module_py(const xll::OPER& name)
 	{
-		std::wstring file(view(module));
-		ensure(file.ends_with(L".xll"));
-		file.replace(file.size() - 4, 4, L"");
-		auto pos = file.find_last_of(L"\\");
-		file = file.substr(pos + 1);
+		std::wstring module(view(name));
+		ensure(module.ends_with(L".xll"));
+		module.replace(module.size() - 4, 4, L"");
+		auto pos = module.find_last_of(L"\\");
+		module = module.substr(pos + 1);
 
-		return file;
+		return module;
 	}
 } // namespace py
 
@@ -154,9 +145,9 @@ int WINAPI xll_make_py()
 {
 #pragma XLLEXPORT
 	try {
-		const xll::OPER module = Excel(xlGetName);
-		const auto file = py::file_py(module);
-		const auto mod = py::module_py(module);
+		const xll::OPER name = Excel(xlGetName);
+		const auto file = py::file_py(name);
+		const auto module = py::module_py(name);
 
 		std::wofstream ofs;
 
@@ -165,7 +156,7 @@ int WINAPI xll_make_py()
 			<< py::install_root
 			<< L"root = install_root()\n"
 			<< L"WinDLL(root + r'\\xlcall32.dll')\n"
-			<< mod << L" = WinDLL(r'" << view(module) << L"')\n\n"
+			<< module << L" = WinDLL(r'" << view(name) << L"')\n\n"
 
 			<< py::XLREF12
 			<< py::SRef
@@ -177,7 +168,7 @@ int WINAPI xll_make_py()
 		for (const auto& [oper, pargs] : xll::AddIns()) {
 			if (pargs->python) {
 				OPER safe = pargs->functionText.make_safe();
-				ofs << std::format(L"{} = getattr({}, r'{}')\n", view(safe), mod, view(pargs->procedure));
+				ofs << std::format(L"{} = getattr({}, r'{}')\n", view(safe), module, view(pargs->procedure));
 				auto res = view(pargs->typeText, 2);
 				if (res[1] != L'%') { // not XLOPER12 type
 					res = res.substr(0, 1);
